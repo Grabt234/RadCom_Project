ea7431418042572c0c92ebc49d38a038c17daf58
Grabt234 <gnorrie12@gmail.com>
2021-07-05 21:13:01 +0200

    changing encoding method

--- functions/processing_chain_rad/utility/load_dab_rad_constants.m
+++ functions/processing_chain_rad/utility/load_dab_rad_constants.m
@@ -24,13 +24,11 @@ function dab_mode = load_dab_rad_constants(transmission_mode)
         dab_mode.T_intra   = 0;
         
     elseif (transmission_mode == 2)
-        %no defined fc
-        %Unused
-        dab_mode.K         = 50;
-        dab_mode.L         = 4;
-        dab_mode.Tnull     = 2656;
-        dab_mode.Tu        = 2048;
-        dab_mode.Tg        = 504;
+        dab_mode.K         = 20;
+        dab_mode.L         = 3;
+        dab_mode.Tnull     = 26560*0.6;
+        dab_mode.Tu        = 20480*0.6;
+        dab_mode.Tg        = 5040*0.6;
         dab_mode.Ts        = dab_mode.Tu + dab_mode.Tg;
         dab_mode.Tf        = dab_mode.Tnull + dab_mode.L * dab_mode.Ts;
         dab_mode.mask      = [dab_mode.Tu/2-dab_mode.K/2 -1:dab_mode.Tu/2,...
---
+++ functions/synthetic_data/ofdm_data_encode/convert_vector_symbols.m
@@ -0,0 +1,55 @@
+function L_encode = convert_vector_symbols(A,dab_mode)
+    % ---------------------------------------------------------------------    
+    % convert_vector_symbols: Takes an array of phase codes and converts
+    %                           them into an array of symbols
+    %                           
+    % ---------------------------------------------------------------------
+    % Usage:
+    %  Inputs
+    %  > A - vector of phase codes
+    %  > dab_mode - describtion of DAB modulation parameters
+    %
+    %  Outputs
+    %  > return n vectors of length L
+    %
+    % ---------------------------------------------------------------------
+    %% ASSIGNING CONSTANTS  
+
+    %carriers per symbol
+    K = dab_mode.K;
+
+    %% CALCULATING REQUIRED PULSES
+
+    %number of complete symbols to be encoded
+    n = ceil(length(A)/K);
+
+    %appending to create integer number of symbols
+    size_filler = n*K - length(A);
+    fill = ones(size_filler,1);
+    
+    %phase codes that are integer number of symbols
+    A = [A; fill];
+
+    %% SPLITTING INTO INDEPENDANT SYMBOLS
+    
+    %does not include null symbol or prs
+    L_encode = reshape(A,[],K);
+   
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
