12e87589f68af836767ee38ffc182c7f898a60c9
Grabt234 <gnorrie12@gmail.com>
2021-07-20 21:44:05 +0200

    issue with coherent integration

--- functions/processing_chain_rad/utility/load_dab_rad_constants.m
+++ functions/processing_chain_rad/utility/load_dab_rad_constants.m
@@ -89,6 +89,20 @@ function dab_mode = load_dab_rad_constants(transmission_mode)
         dab_mode.p_intra   = 1;
         dab_mode.T_intra   = 0;
         dab_mode.Tf        = (dab_mode.Tp + dab_mode.T_intra)*dab_mode.p_intra;
+    elseif (transmission_mode == 7)
+        %no defined fc
+        dab_mode.K         = 30;
+        dab_mode.L         = 2;
+        dab_mode.Tnull     = 2552;
+        dab_mode.Tu        = 2048;
+        dab_mode.Tg        = 504;
+        dab_mode.Ts        = dab_mode.Tu + dab_mode.Tg;
+        dab_mode.Tp        = dab_mode.Tnull + (dab_mode.L)*dab_mode.Ts;
+        dab_mode.mask      = [ (dab_mode.Tu/2-dab_mode.K/2 +1):(dab_mode.Tu/2), ...
+                                        (dab_mode.Tu/2+2):(dab_mode.Tu/2+dab_mode.K/2 +1) ];
+        dab_mode.p_intra   = 2;
+        dab_mode.T_intra   = dab_mode.Tp;
+        dab_mode.Tf        = (dab_mode.Tp + dab_mode.T_intra)*dab_mode.p_intra;
     else
         fprintf("Transmission Mode %d is either invalid or has not yet been implemented.\n", ...
             transmission_mode);
